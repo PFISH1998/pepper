1389873bf6286ad9caabddceb2c8b65f131c512d
pfish1998 <perry1998@outlook.com>
2019-05-31 20:55:29 +0800

    Initial commit

---
+++ index/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# 舆情事件数据
+class Event(models.Model):
+    # 事件标题
+    title = models.CharField(max_length=50, null=False)
+    # 创建事件时间
+    created_time = models.DateTimeField()
+    # 备注或标记
+    mark = models.CharField(max_length=50)
+    # 关键词
+    keyword = models.CharField(max_length=50)
+
